b6946af6f92a82848b37c12c0c601c090912dfc2
Diego Gonzalez <kaoru17th@gmail.com>
2013-08-16 11:34:35 -0500

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CalcularPesoApp::Application.config.secret_key_base = 'ff940352947cc2ca878d735a05cc2f868810a812f331f6382cd7eef417c9f371b5ea0a5b3fd3eafc496cad12edc1fe0b226f2bbbf10bf6bc152b2193ce6c91bd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CalcularPesoApp::Application.config.session_store :cookie_store, key: '_calcular_peso_app_session'
